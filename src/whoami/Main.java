73d30095fb369fcdd8704c86512c3df02af72517
Pedro404r <jessicaulissesalbardeiro@gmail.com>
2025-09-18 18:00:55 -0300

    projeto calculadora do exercicio 1

--- src/whoami/Main.java
+++ src/whoami/Main.java
@@ -5,45 +5,39 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        String[] perguntas = new String[4];
-        perguntas[0] = "Qual o nome do ser denominado de 'Bigodinho'?";
-        perguntas[1] = "Quando ocorrou a primeira guerra mundial";
-        perguntas[2] = "Qual o nome para cachorros mais comum no Brasil?";
-        perguntas[3] = "Qual o sobrenome do maior apresentador brasileiro já visto?";
+        // calculadora simples
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número:");
+        double num1 = scanner.nextDouble();
+        System.out.println("Digite o segundo número:");
+        double num2 = scanner.nextDouble();
+        System.out.println("Escolha a operação (+, -, *, /):");
+        String operacao = scanner.next();
+        double resultado = 0;
+        switch (operacao) {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            default:
+                System.out.println("Operação inválida!");
+                return;
 
-        String[] respostas = new String[4];
-        respostas[0] = "Hitler";
-        respostas[1] = "1945";
-        respostas[2] = "Caramelo";
-        respostas[3] = "Santos";
 
-        // lista de strings
-        List<String> maisQuestoes = new ArrayList<>();
-        maisQuestoes.add("Qual o nome do maior jogador de futebol de todos os tempos?");
-        maisQuestoes.add("Qual o nome do maior jogador de basquete de todos os tempos?");
-        maisQuestoes.add("Nome da pior deputada trans brasileira");
-    //  maisQuestoes.clear();
-    
-        Set<String> PaisesAVisitar = new HashSet<>();
-        PaisesAVisitar.add("Japão");
-        PaisesAVisitar.add("Canadá");
-        PaisesAVisitar.add("Inglaterra");
-
-        System.out.println(PaisesAVisitar);
-
-        Map<String, Integer> populacaoCadaPais = new HashMap<>();
-        populacaoCadaPais.put("Japão", 125000000);
-        populacaoCadaPais.put("Canadá", 38000000);
-        populacaoCadaPais.put("Inglaterra", 56000000);
-
-        System.out.println(populacaoCadaPais.get("Japão"));
-
-        for(int i = 0; i < maisQuestoes.size(); i++) {
-            System.out.println(maisQuestoes.get(i));
-        }
     }
-}
+        System.out.println("Resultado: " + resultado);
+        scanner.close();
+}}
